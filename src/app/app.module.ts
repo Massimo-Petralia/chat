1054256ce95137c59b5a5f88afee0eed51019b83
massimo <massipetra@hotmail.it>
2023-10-31 16:55:43 +0100

    feat: add navigation to user-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatLoginComponent } from './features/chat/chat-login/chat-login.component';
 import { ChatPageComponent } from './features/chat/chat-page/chat-page.component';
+import { ChatUserListComponent } from './features/chat/chat-user-list/chat-user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatLoginComponent,
-    ChatPageComponent
+    ChatPageComponent,
+    ChatUserListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      {path: '', component: ChatLoginComponent}
+      {path: '', component: ChatLoginComponent},
+      {path: 'user-list', component: ChatUserListComponent}
     ])
   ],
   providers: [],
--- src/app/features/chat/chat-page/chat-page.component.ts
+++ src/app/features/chat/chat-page/chat-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-chat-page',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat-page.component.scss']
 })
 export class ChatPageComponent {
+
+  subs = new Subscription()
+
+  onLog(user: User) {
+
+  }
 
 }
